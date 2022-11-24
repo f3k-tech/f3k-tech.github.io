f39fa194c8141082270b4deeb00c5e0710c6d079
FreekO <electronics@amaze-escape.com>
2022-11-24 13:03:21 +0300

    fix download script

--- bin/download-bs.sh
+++ bin/download-bs.sh
@@ -4,5 +4,5 @@ VERSION='5.2.3'
 
 wget "https://github.com/twbs/bootstrap/archive/v$VERSION.zip"
 unzip "v$VERSION.zip"
-mv "bootstrap-$VERSION" "_sass/bootstrap"
-rm -r "v$VERSION.zip"
+mv "bootstrap-$VERSION/scss" "_sass/bootstrap"
+rm -r "v$VERSION.zip" "bootstrap-$VERSION"
