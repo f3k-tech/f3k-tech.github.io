25a348c1b5e9f0d186a831cc6fff6d38ba3f9be0
FreekO <electronics@amaze-escape.com>
2022-11-24 12:58:20 +0300

    fix bootstrap script

--- bin/download-bs.sh
+++ bin/download-bs.sh
@@ -2,7 +2,7 @@
 
 VERSION='5.2.3'
 
-wget "https://github.com/twbs/bootstrap/releases/download/v$VERSION/bootstrap-$VERSION-dist.zip"
-unzip "bootstrap-$VERSION-dist.zip"
-mv "bootstrap-$VERSION-dist.zip" "_sass/bootstrap"
-rm -r "bootstrap-$VERSION-dist"
+wget "https://github.com/twbs/bootstrap/archive/v$VERSION.zip"
+unzip "v$VERSION.zip"
+mv "bootstrap-$VERSION" "_sass/bootstrap"
+rm -r "v$VERSION.zip"
