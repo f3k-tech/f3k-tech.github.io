85815b15e6c7892125ccf0a8b705b090e78b63e2
FreekO <electronics@amaze-escape.com>
2022-11-24 13:49:53 +0300

    add download js file to download script

--- bin/download-bs.sh
+++ bin/download-bs.sh
@@ -6,3 +6,5 @@ wget "https://github.com/twbs/bootstrap/archive/v$VERSION.zip"
 unzip "v$VERSION.zip"
 mv "bootstrap-$VERSION/scss" "_sass/bootstrap"
 rm -r "v$VERSION.zip" "bootstrap-$VERSION"
+
+wget "https://cdn.jsdelivr.net/npm/bootstrap@$VERSION/dist/js/bootstrap.bundle.min.js" -O "assets/js/bootstrap.bundle.min.js"
